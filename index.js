fcf5bb56d5b162ee9ce3d3457be3dfffecd720ce
shinigamisam <saima.farzina39@gmail.com>
2026-08-08 03:31:51 +0600

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import healthRoutes from "./routes/health.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.use("/health", healthRoutes);
+
+const info = {
+  name: "Task API",
+  version: "1.0",
+  endpoints: ["/tasks"],
+};
 
 app.get("/", (req, res) => {
   console.log("Home route");
-  res.status(200).json({ message: "Hello" });
+  res.status(200).json(info);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
