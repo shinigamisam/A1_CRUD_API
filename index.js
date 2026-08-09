565dd58c9b8e2951f8c9d008c0d0fd027f76ff1d
shinigamisam <saima.farzina39@gmail.com>
2026-08-10 01:49:54 +0600

    Stage 5: Swagger UI

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import healthRoutes from "./routes/health.js";
 import taskRoutes from "./routes/tasks.js";
+import swaggerUi from "swagger-ui-express";
+import openapiDocument from "./openapi.json" with { type: "json" };
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +11,7 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use("/tasks", taskRoutes);
 app.use("/health", healthRoutes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiDocument));
 
 const info = {
   name: "Task API",
